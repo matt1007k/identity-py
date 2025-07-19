814c3384eb1f7681e5633f163d154f2121e666ab
Max Meza <matt1007k@gmail.com>
2025-07-19 15:55:01 -0500

    commet headless for RUC scraping

--- src/core/util.py
+++ src/core/util.py
@@ -11,7 +11,7 @@ def createDriver() -> webdriver.Chrome:
         options = webdriver.ChromeOptions()
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--disable-dev-shm-usage")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
